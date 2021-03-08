2b193833710d175a4d6d7880d98339f8e238b8fb
crazylads <74870456+Prabal-Rawal@users.noreply.github.com>
2021-03-08 22:37:54 +0530

    Update tables-config.py

--- tables-config.py
+++ tables-config.py
@@ -6,16 +6,19 @@ cur = db.cursor()
 
 
 def table():
-    cur.execute(
-    "CREATE TABLE customer_data(ID int(4) Primary Key, NAME varchar(15),AGE integer, ADDRESS varchar(50), E_MAIL varchar(40))")
-    cur.execute("CREATE TABLE BILL(BILL_NO int(10), ID int(4) Primary Key , CHECK_IN varchar(10), CHECK_OUT varchar(10), NO_OF_ROOM INT(5), ROOM_TYPE varchar(20),TOTAL int(10))")
+    try:
+        cur.execute("CREATE TABLE customer_data(ID int(4) Primary Key, NAME varchar(15),AGE integer, ADDRESS varchar(50), E_MAIL varchar(40))")
+        bill()
+    except:
+        print("tables error1")
+    
+
+    
+def bill():
+    cur.execute("CREATE TABLE BILL(BILL_NO int(10), ID int(4) Primary Key , CHECK_IN varchar(10), CHECK_OUT varchar(10),NO_OF_DAYS int(10), NO_OF_ROOM INT(5), ROOM_TYPE varchar(20),TOTAL int(10))")
     db.commit()
-
-    
-    print("TABLES customer_data created sucessfully!!")
-
-    
-
+    print("Tables created sucessfully")
 
 
 table()
+
